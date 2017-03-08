01f72d437bd7eafac7f9e39d38421eb1dc6401a1
ThomasMonkman <tommonkman@hotmail.co.uk>
2017-03-08 22:09:51 +0000

    Working on adding functionality for listening to the network

---
+++ include/BaseListener.hpp
@@ -0,0 +1,23 @@
+#ifndef BASELISTENER_HPP
+#define BASELISTENER_HPP
+
+#include <string>
+#include <functional>
+#include <utility>
+
+namespace Seer {
+	class BaseListener
+	{
+	public:
+		BaseListener(std::string name, std::function<void()> callback) : 
+			_name(name),
+			_callback(std::move(_callback))
+		{
+		}
+		virtual ~BaseListener();
+	private:
+		std::string _name;
+		std::function<void()> _callback
+	};
+}
+#endif
