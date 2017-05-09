37e580da84f9ba1c410ac6dc2c16be8ead50453a
ThomasMonkman <tommonkman@hotmail.co.uk>
2017-05-10 00:49:52 +0100

    start on test helper sink to allow inspection and sniffing of the pipe, this will help write tests, allowing you to check if the json has moved through the network at expected.

---
+++ tests/src/HelperSink.hpp
@@ -0,0 +1,41 @@
+#ifndef TESTHELPERSINK_HPP
+#define TESTHELPERSINK_HPP
+
+#include "../../include/Seer.hpp"
+
+#include "../../3rdParty/json/json.hpp"
+
+#include <future>
+#include <string>
+#include <utility>
+
+class TestHelperSink : Seer::BaseSink
+{
+public:
+	TestHelperSink()
+	{
+
+	};
+	~TestHelperSink() override {};
+
+	void send(const std::string& data) override
+	{		
+		auto json = nlohmann::json::parse(data);
+		auto match = _matches.find(json["#"]);
+		if (match != _matches.end()) {
+			match->second.set_value(json);
+			_matches.erase(match);
+		}
+	}
+
+	std::future<nlohmann::json> get_match(std::string message_type) {
+		auto promise = std::promise<nlohmann::json>();
+		auto future = promise.get_future();
+		_matches[message_type] = std::move(promise);
+		return future;
+	}
+private:
+	std::map<std::string, std::promise<nlohmann::json>> _matches;
+};
+
+#endif
