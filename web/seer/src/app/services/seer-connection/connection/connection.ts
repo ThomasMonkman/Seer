af46e9c51f78425d913c32fd04552463d9ffa7cc
ThomasMonkman <tommonkman@hotmail.co.uk>
2017-07-05 00:20:51 +0100

    Added better web socket implementation

--- web/seer/src/app/services/seer-connection/connection/connection.ts
+++ web/seer/src/app/services/seer-connection/connection/connection.ts
@@ -1,19 +1,112 @@
-import { QueueingSubject } from 'queueing-subject'
-import { Observable } from 'rxjs/Rx';
-import websocketConnect from 'rxjs-websockets'
+// https://gearheart.io/blog/auto-websocket-reconnection-with-rxjs/
+import { Subject, Observer, Observable } from 'rxjs';
+import { WebSocketSubject, WebSocketSubjectConfig } from 'rxjs/observable/dom/WebSocketSubject';
 
-export class Connection {
-    public readonly url: string;
-    public readonly inputStream: QueueingSubject<any>;
-    public readonly messages: Observable<any>;
-    constructor(url) {
-        this.url = url;
-        this.inputStream = new QueueingSubject<any>();
-        this.messages = websocketConnect(this.url, this.inputStream).messages;
-        // this.messages = this.messages.retryWhen(errors => errors.delay(1000));
+// we inherit from the ordinary Subject
+export class RxWebsocketSubject<T> extends Subject<T> {
+    private reconnectionObservable: Observable<number>;
+    private wsSubjectConfig: WebSocketSubjectConfig;
+    private socket: WebSocketSubject<any>;
+    private connectionObserver: Observer<boolean>;
+    public readonly connectionStatus: Observable<boolean>;
+
+    // by default, when a message is received from the server, we are trying to decode it as JSON
+    // we can override it in the constructor
+    private readonly defaultResultSelector = (e: MessageEvent) => {
+        return JSON.parse(e.data);
     }
 
-    public send(message: any): void {
-        this.inputStream.next(message);
+    // when sending a message, we encode it to JSON
+    // we can override it in the constructor
+    private readonly defaultSerializer = (data: any): string => {
+        return JSON.stringify(data);
+    }
+
+    constructor(
+        private readonly url: string,
+        private readonly reconnectInterval: number = 5000,  /// pause between connections
+        private readonly reconnectAttempts: number = Number.MAX_SAFE_INTEGER,  /// number of connection attempts
+
+        private readonly resultSelector?: (e: MessageEvent) => any,
+        private readonly serializer?: (data: any) => string,
+    ) {
+        super();
+
+        // connection status
+        this.connectionStatus = new Observable((observer) => {
+            this.connectionObserver = observer;
+        }).share().distinctUntilChanged();
+
+        if (!resultSelector) {
+            this.resultSelector = this.defaultResultSelector;
+        }
+        if (!this.serializer) {
+            this.serializer = this.defaultSerializer;
+        }
+
+        // config for WebSocketSubject
+        // except the url, here is closeObserver and openObserver to update connection status
+        this.wsSubjectConfig = {
+            url: url,
+            closeObserver: {
+                next: (e: CloseEvent) => {
+                    this.socket = null;
+                    this.connectionObserver.next(false);
+                }
+            },
+            openObserver: {
+                next: (e: Event) => {
+                    this.connectionObserver.next(true);
+                }
+            }
+        };
+        // we connect
+        this.connect();
+        // we follow the connection status and run the reconnect while losing the connection
+        this.connectionStatus.subscribe((isConnected) => {
+            if (!this.reconnectionObservable && typeof (isConnected) == "boolean" && !isConnected) {
+                this.reconnect();
+            }
+        });
+    }
+
+    // sending the message
+    public send(data: any): void {
+        this.socket.next(this.serializer(data));
+    }
+
+    private connect(): void {
+        this.socket = new WebSocketSubject(this.wsSubjectConfig);
+        this.socket.subscribe(
+            (m) => {
+                this.next(m); // when receiving a message, we just send it to our Subject
+            },
+            (error: Event) => {
+                if (!this.socket) {
+                    // in case of an error with a loss of connection, we restore it
+                    this.reconnect();
+                }
+            });
+    }
+
+    // reconnection
+    private reconnect(): void {
+        this.reconnectionObservable = Observable.interval(this.reconnectInterval)
+            .takeWhile((v, index) => {
+                return index < this.reconnectAttempts && !this.socket
+            });
+        this.reconnectionObservable.subscribe(
+            () => {
+                this.connect();
+            },
+            null,
+            () => {
+                // if the reconnection attempts are failed, then we call complete of our Subject and status
+                this.reconnectionObservable = null;
+                if (!this.socket) {
+                    this.complete();
+                    this.connectionObserver.complete();
+                }
+            });
     }
 }
--- web/seer/src/app/services/seer-connection/seer-connection.service.ts
+++ web/seer/src/app/services/seer-connection/seer-connection.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Connection } from './connection/connection';
+import { RxWebsocketSubject } from './connection/connection';
 import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class SeerConnectionService {
-  private connections: Map<string, Connection> = new Map<string, Connection>();
+  private connections: Map<string, RxWebsocketSubject<object>> = new Map<string, RxWebsocketSubject<object>>();
   constructor() { }
 
   public connect(url: string): Observable<any> {
     if (this.connections.has(url)) {
-      return this.connections.get(url).messages;
+      return this.connections.get(url);
     } else {
-      const connection = new Connection(url);
+      const connection = new RxWebsocketSubject(url);
       this.connections.set(url, connection);
-      return connection.messages;
+      return connection;
     }
-  }  
+  }
 
   public send(url: string, message: any): void {
-     if (this.connections.has(url) === false) {
+    if (this.connections.has(url) === false) {
       throw new Error(`No connection by the url: ${url} exists, please call connect first`);
     }
     this.connections.get(url).send(message);
