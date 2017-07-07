54bbdbfee00e35a8408b3eacd78ea7424388e2b2
ThomasMonkman <tommonkman@hotmail.co.uk>
2017-07-07 01:01:03 +0100

    Further work, trying to get past connections to highlight when input fields match or when they are clicked

--- web/seer/src/app/views/dashboard/new-connection/new-connection.component.ts
+++ web/seer/src/app/views/dashboard/new-connection/new-connection.component.ts
@@ -9,28 +9,20 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: './new-connection.component.html',
   styleUrls: ['./new-connection.component.scss']
 })
-export class NewConnectionComponent implements OnInit {
+export class NewConnectionComponent {
   protected connectionForm = new FormGroup({
     address: new FormControl('', [Validators.required]),
     name: new FormControl(''),
   });
 
-  protected filteredPastConnections: ConnectionMeta[] = [];
+  protected filteredPastConnections: ConnectionMeta[] = [];// = [new ConnectionMeta('hello', 'hello')];
 
   constructor(private connectionHistory: ConnectionHistoryService) {
-    connectionHistory.store(new ConnectionMeta('localhost', 'localhost'));
-    // connectionHistory.get().subscribe((connections) => {
-    //   this.pastConnections = connections;
-    // });
-
     Observable.combineLatest(
       this.connectionHistory.get(),
       this.connectionForm.get('address').valueChanges.startWith(null),
       this.connectionForm.get('name').valueChanges.startWith(null)
-    ).subscribe(this.filterHistory);
-  }
-
-  public ngOnInit() {
+    ).subscribe(val => this.filterHistory(val));
   }
 
   protected connect() {
@@ -39,13 +31,21 @@ export class NewConnectionComponent implements OnInit {
     }
   }
 
+  protected historySelected(connection: ConnectionMeta) {
+    this.connectionForm.setValue({ address: connection.address, name: connection.name });
+  }
+
+  protected historySameAsInput(connection: ConnectionMeta): boolean {
+    return this.connectionForm.get('address').value === connection.address && this.connectionForm.get('name').value === connection.name;
+  }
+
   private filterHistory(val: [ConnectionMeta[], string, string]) {
     const [pastConnections, address, name] = val;
+    //need to add sort as well
     this.filteredPastConnections = pastConnections.filter(connection => {
       let addressFilter = address ? connection.address.toLowerCase().includes(address.toLowerCase()) : true;
       let nameFilter = name ? connection.name.toLowerCase().includes(name.toLowerCase()) : true;
       return addressFilter || nameFilter;
     });
-    console.log(this.filteredPastConnections);
   }
 }
